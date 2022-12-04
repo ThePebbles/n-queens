0f8d537425c551c41c89e9153669106615e6837e
Sarah <sarahkwando@gmail.com>
2022-12-03 21:15:39 -0600

    Complete N-Rooks

--- src/solvers.js
+++ src/solvers.js
@@ -29,7 +29,6 @@ window.findNRooksSolution = function(n) {
     return checkCoors;
 
   };
-
   checkCoors = getCoors(n);
   var solution = new Board({n: n});
   var found = 0;
@@ -37,18 +36,10 @@ window.findNRooksSolution = function(n) {
     if (found === n) {
       return b;
     }
-
     var coor = checkCoors.pop();
     var c1 = Number(coor.charAt(0));
     var c2 = Number(coor.charAt(1));
-
-    //add piece at coordinate
-    //check if any issues
-    //recursion
-
-
     b.togglePiece(c1, c2);
-
     if (!b.hasAnyRooksConflicts()) {
       found++;
     } else {
@@ -58,13 +49,6 @@ window.findNRooksSolution = function(n) {
     return addRooks(b);
 
   };
-  // get random numbers each time recursive function runs
-  // check numbers are new/have not been tried
-  // toggle piece at random number coordinates on copy board
-  // run check rook conflicts on copy board if conflict then board = board
-  // if no conflict the board = copy board
-  // call function again with board
-
   solution = addRooks(solution);
   var output = [];
   for (var x = 0; x < n; x++) {
@@ -76,80 +60,43 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = 0; //fixme
-
+  var solutionCount = 0;
   var board = new Board({n: n});
-  //Make array of all combinations of n pieces
-  //for
-  var possiblePlacements = makePossibleCombinations(n);
-  for (var x = 0; x < possiblePlacements.length; x++) {
-    //toggle
-    //check for conflicts
-    //add one to solutionCount if no conflicts
-    //untoggle
-    var passOn = false;
-    for (var coor of possiblePlacements[x]) {
-      board.togglePiece(coor[0], coor[1]);
-      if (board.hasAnyRooksConflicts()) {
-        board.clearBoard();
-        passOn = true;
-        break;
+  var occupied = [];
+  var solutions = function(n, row, column) {
+    for (column; column < n; column++) {
+      if (occupied.indexOf(column) > -1) {
+        continue;
       }
+      board.togglePiece(row, column); // toggle on
+      console.log('Board for: ', n, board);
+      if (row !== n - 1) {
+        occupied.push(column);
+        solutions(n, row + 1, 0);
+      } else if (row === n - 1) {
+        occupied.push(column);
+        solutionCount++;
+      }
+      board.togglePiece(row, column);
+      occupied.pop();
     }
-    if (!board.hasAnyRooksConflicts() && !passOn) {
-      solutionCount++;
-    }
-    board.clearBoard();
-    //Would that work? toggle the pieces and then undo them once we check?
-    //I believe so, wondering if we can make it more legible, but it doesn't matter as long as it works!
-    //Lemme know if it works on our specs... did it work?
-  }
-
+    return solutionCount;
+  };
+  solutions(n, 0, 0);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var checkCoors = [];
-  var getCoors = function(n) {
-    for (var x = 0; checkCoors.length < n * n; x++) {
-      var c1 = Math.floor(Math.random() * n);
-      var c2 = Math.floor(Math.random() * n);
-      if (!checkCoors.includes('' + c1 + c2)) {
-        checkCoors.push('' + c1 + c2);
-      }
-    }
-    return checkCoors;
-  };
+  if (n === 2 || n === 3) {
+    return 0;
+  }
+  var board = new Board({n: n});
 
-  checkCoors = getCoors(n);
-  var solution = new Board({n: n});
-  var found = 0;
-  var addQueens = function(b) {
-    if (found === n) {
-      return b;
-    }
-    var coor = checkCoors.pop();
-    var c1 = Number(coor.charAt(0));
-    var c2 = Number(coor.charAt(1));
-
-    b.togglePiece(c1, c2);
-
-    if (!b.hasAnyQueensConflicts()) {
-      found++;
-    } else {
-      b.togglePiece(c1, c2);
-    }
-
-    return addQueens(b);
-
-  };
-
-  solution = addQueens(solution);
   var output = [];
   for (var x = 0; x < n; x++) {
-    output.push(solution.attributes[x]);
+    output.push(board.attributes[x]);
   }
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(output));
   return output;
@@ -157,70 +104,39 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  if (n === 0) {
+    return 1;
+  }
+  if (n === 2 || n === 3) {
+    return 0;
+  }
+  var solutionCount = 0;
+  var board = new Board({n: n});
+  var occupied = [];
+  var lastColumn;
+  var solutions = function(n, row, column) {
+    for (column; column < n; column++) {
+      var occupy = occupied.indexOf(column);
+      if (occupy > -1) {
+        continue;
+      }
+      board.togglePiece(row, column); // toggle on
+      console.log('Board for: ', n, board);
+      if (row !== n - 1 && !board.hasAnyQueensConflicts()) {
+        occupied.push(column);
+        lastColumn = column;
+        solutions(n, row + 1, 0);
+      } else if (row === n - 1 && !board.hasAnyQueensConflicts()) {
+        occupied.push(column);
+        solutionCount++;
+      }
+      board.togglePiece(row, column);
+      occupied.pop();
+    }
+    return solutionCount;
+  };
+  solutions(n, 0, 0);
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
-
-
-window.makePossibleCombinations = function(n) {
-  //PossibleSquares = [[row1 Squares] [row2 Squares] etc...]
-  // var possibleSquares = [];
-  // var board = new Board({n: n});
-  // for (var row = 0; row < n; row++) {
-  //   possibleSquares[row] = [];
-  //   for (var column = 0; column < n; column++) {
-  //     possibleSquares[row].push([row, column]);
-  //   }
-  // }
-
-  var outputs = [];
-  var columns = [];
-  for (var x = 0; x < n; x++) {
-    columns[x] = 0;
-  }
-
-  var chooseN = function(columns) {
-    if (columns === 'done') {
-      return;
-    }
-
-    outputList = [];
-
-    for (var x = 0; x < n; x++) {
-      outputList.push([x, columns[x]]);
-    }
-    outputs.push(outputList);
-
-
-    var updateColumns = function(l, c) {
-      if (l.every(function(item) {
-        if (item === n - 1) {
-          return true;
-        }
-      })) {
-        return 'done';
-      }
-      l[c] += 1;
-      if (l[c] >= n) {
-        if (c !== 0) {
-          l[c] = 0;
-          c -= 1;
-          updateColumns(l, c);
-        } else {
-          l[c] -= 1;
-          return l;
-        }
-      }
-      return l;
-    };
-
-    chooseN(updateColumns(columns, n - 1));
-
-  };
-  chooseN(columns);
-  return outputs;
-};
-
-console.log(makePossibleCombinations(3));
